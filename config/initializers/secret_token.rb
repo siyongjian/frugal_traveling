0ee5006e7ba0e85a35b4eefec2fca5693f5816a0
Yongjian Si <si.yongjian@gmail.com>
2014-07-22 01:40:54 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FrugalTraveling::Application.config.secret_key_base = 'e507b45e085673ceefad9998567d72ad2a2e60f8a5beb00c8797351891320007b695e59966605afe2523277041c54340c8b6cf9a805ce646a74c247b10fbd49e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FrugalTraveling::Application.config.session_store :cookie_store, key: '_frugal_traveling_session'
